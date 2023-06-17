518695bf41e2906c777f8d4c690e6dc1d8b94447
Sona0511 <134961968+Sona0511@users.noreply.github.com>
2023-06-17 20:36:04 +0400

    Update rotate_by.py

--- rotate_by.py
+++ rotate_by.py
@@ -1,2 +1,9 @@
 #Implement the rotate_by() function taking an integer array and a number as its arguments. 
 #The function returns the array rotated by the specified number of positions (given by the second argument).
+def rotate_by(array, number):
+    rotated = array[number:] + array[:number]
+    return rotated
+
+# Example
+array = [8, 1, 6, 5, 2]
+print(rotate_by(array, 3))  

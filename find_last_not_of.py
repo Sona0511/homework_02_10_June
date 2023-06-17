3455b2ec7fd818eaef9eed6d116601996ebc5805
Sona0511 <134961968+Sona0511@users.noreply.github.com>
2023-06-17 19:02:01 +0400

    Update find_last_not_of.py

--- find_last_not_of.py
+++ find_last_not_of.py
@@ -1,3 +1,13 @@
 #Implement the find_last_not_of() function taking a string and a character sequence as its arguments. 
 #The function returns the last character equal to none of the characters in the given character sequence.
+def find_last_not_of(string, chars):
+    for char in reversed(string):
+          if char not in chars:
+              return char
+    return "false"   
+    
+#Example
+string = "abc def ghi"
+chars = "def ghi" 
 
+print(find_last_not_of(string, chars))  

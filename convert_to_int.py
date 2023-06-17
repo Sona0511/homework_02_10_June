dc5704114c9fd598dd1ee45bd2fcd658e22557af
Sona0511 <134961968+Sona0511@users.noreply.github.com>
2023-06-17 23:05:41 +0400

    Update convert_to_int.py

--- convert_to_int.py
+++ convert_to_int.py
@@ -1,2 +1,13 @@
 #Implement the convert_to_int() function taking a string argument and returning its integer representation. 
 #Throw an exception if the conversion is not possible.
+def convert_to_int(string):
+    try:
+        integer=int(string)
+        return integer
+    except ValueError:
+        raise Exception("not possible")
+
+
+#example
+string="14"
+print(convert_to_int(string))

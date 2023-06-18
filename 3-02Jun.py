b3162769edd2f5229d857545c249c910a9d5191a
Sona0511 <134961968+Sona0511@users.noreply.github.com>
2023-06-18 20:31:15 +0400

    Update 3-02Jun.py

--- 3-02Jun.py
+++ 3-02Jun.py
@@ -6,3 +6,15 @@
 # How many hours ahead? 5
 # New hour: 1 pm
 
+hour=int(input("Enter hour now(1-12): "))
+time_period=input("Advise time period: am(1) or pm(2): ")
+hours_ahead=int(input("How many hours into the future do you want to go?: "))
+
+new_hour= (hour+hours_ahead)
+
+if time_period == "1" and new_hour>=12:
+    new_period = "2"
+else:
+    new_period=time_period
+
+print("New hour:", new_hour%12, "pm" if new_period == '2' else "am")

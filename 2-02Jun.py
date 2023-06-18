6c65f76af0ec26e541d591ef99a849867f461ae4
Sona0511 <134961968+Sona0511@users.noreply.github.com>
2023-06-18 18:37:28 +0400

    Update rotate matrix.py

--- 2-02Jun.py
+++ 2-02Jun.py
@@ -10,3 +10,14 @@
 #8   7   6   5
 #4   3   2   1
 
+def rotate_matrix_180(matrix):
+    rotated_matrix = [line[::-1] for line in matrix[::-1]]
+    return rotated_matrix
+
+#Input
+matrix = [[1, 2, 3, 4],
+          [5, 6, 7, 8],
+          [9, 10, 11, 12],
+          [13, 14, 15, 16]]
+
+[print(line) for line in rotate_matrix_180(matrix)]
